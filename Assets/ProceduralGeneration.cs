be3f46386108fc9703a7f0ca5e52d76ab23f12e9
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-01-08 19:00:52 +0100

    Remade Terrain Gen

---
+++ Assets/ProceduralGeneration.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+   [SerializeField] int width,height;
+   [SerializeField]float smoothness;
+   [SerializeField]float seed;
+   [SerializeField]TileBase groundTile,caveTile;
+   [SerializeField]Tilemap groundTileMap,caveTileMap;
+   int[,] map;
+
+    [Range(0,1)]
+    [SerializeField] float modifier;
+   
+   void Start()
+   {
+       Generation();
+   }
+
+   void Update()
+   {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Generation();
+        }       
+   }
+
+   void Generation(){
+    seed=Random.Range(-10000,10000);
+    map=GenerateArray(width,height,true);
+    map=TerrainGeneration(map);
+    RenderMap(map,groundTileMap,caveTileMap,groundTile,caveTile);
+   }
+
+   public int[,] GenerateArray(int width,int height,bool empty){
+    int [,] map=new int[width,height];
+    for(int x=0;x<width;x++){
+        for(int y=0;y<height;y++){
+            map[x,y]=(empty)? 0:1;
+        }
+    }
+    return map;
+   }
+   public int[,] TerrainGeneration(int[,] map){
+    int perlinHeight;
+    for(int x=0;x<width;x++){
+        perlinHeight=Mathf.RoundToInt(Mathf.PerlinNoise(x/smoothness,seed)*height/2);
+        perlinHeight += height / 2;
+        for(int y=0;y<perlinHeight;y++){
+            //map[x,y]=1;
+            int caveValue=Mathf.RoundToInt(Mathf.PerlinNoise((x*modifier)+seed,(y*modifier)+seed));
+            map[x,y]=(caveValue==1) ? 2:1;
+        }
+    }
+    return map;
+   }
+
+   public void RenderMap(int[,] map,Tilemap groundTileMap,Tilemap caveTileMap,TileBase groundTileBase,TileBase caveTileBase){
+    for(int x=0;x<width;x++){
+        for(int y=0;y<height;y++){
+            if(map[x,y]==1){
+                groundTileMap.SetTile(new Vector3Int(x,y,0),groundTileBase);
+            }else if(map[x,y]==2){
+                caveTileMap.SetTile(new Vector3Int(x,y,0),caveTileBase);
+            }
+        }
+    }
+   }
+}
